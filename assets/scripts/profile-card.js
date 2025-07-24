8b372c5c3a5d940094babac99676d7e66579c5fb
Rasmus Olsen <55601813+52534F@users.noreply.github.com>
2025-07-24 10:50:50 +0200

    Update profile-card.js

--- assets/scripts/profile-card.js
+++ assets/scripts/profile-card.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const profiles = document.querySelectorAll('.profile');
-    const fullscreen = document.querySelector('.fullscreen');
+    const profileFullscreen = document.querySelector('.profile-fullscreen');
     const closeButton = fullscreen.querySelector('.close-button');
 
     profiles.forEach(profile => {
         profile.addEventListener('click', () => {
             // Activerer fullscreen view med profilinnhold
-            fullscreen.classList.add('active');
+            profileFullscreen.classList.add('active');
             
             // Henter profil detalj-innhold
             const detailText = profile.dataset.description;
             const detailImageSrc = profile.querySelector('img').src;
             
             // Setter inn innhold i fullscreen visningen
-            fullscreen.querySelector('#profile-detail-text').textContent = detailText;
-            fullscreen.querySelector('#profile-image').src = detailImageSrc;
-            fullscreen.querySelector('#profile-name').textContent = profile.querySelector('h3').textContent;
+            profileFullscreen.querySelector('#profile-detail-text').textContent = detailText;
+            profileFullscreen.querySelector('#profile-image').src = detailImageSrc;
+            profileFullscreen.querySelector('#profile-name').textContent = profile.querySelector('h3').textContent;
         });
     });
 
     closeButton.addEventListener('click', () => {
         // Lukker fullscreen view
-        fullscreen.classList.remove('active');
+        profileFullscreen.classList.remove('active');
     });
 });
