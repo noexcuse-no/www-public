e61b21ea052ca28e459000ee91635648477a4953
Rasmus Olsen <55601813+52534F@users.noreply.github.com>
2025-07-26 00:48:59 +0200

    Update profile-card.js

--- assets/scripts/profile-card.js
+++ assets/scripts/profile-card.js
@@ -2,17 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const profiles = document.querySelectorAll('.profile-compact');
     const profileExpanded = document.querySelector('.profile-expanded');
     const closeButton = profileExpanded.querySelector('.close-button');
-
+    const profileCompact = document.querySelector('.profile-compact.inactive');
+    
     profiles.forEach(profile => {
         profile.addEventListener('click', () => {
-            const profileCompact = profile.querySelector('.profile-compact');
             profileExpanded.classList.add('active');
             profile.classList.add('inactive');
         });
     });
 
     closeButton.addEventListener('click', () => {
-        // Lukker fullscreen view
         profileExpanded.classList.remove('active');
         profileCompact.classList.remove('inactive');
     });
