f7774b835f842225722b1bad7792b812c949fa9b
Rasmus Olsen <55601813+52534F@users.noreply.github.com>
2025-07-26 02:14:18 +0200

    Update profile-card.js

--- assets/scripts/profile-card.js
+++ assets/scripts/profile-card.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const profileCollapsed = profile.querySelector('.profile-compact');
             const profileExpanded = profile.querySelector('.profile-expanded');
             profilesExpanded.forEach(n => { n.classList.remove('active'); });
+            profilesCollapsed.forEach(n => { n.classList.remove('inactive'); });
             profileCollapsed.classList.add('inactive');
             profileExpanded.classList.add('active');
         });
